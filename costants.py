5443edb2a21957244c8d7853bc99a5709d3f5b46
Niran Amos <niamos@microsoft.com>
2023-07-31 13:08:27 +0300

    readme

--- costants.py
+++ costants.py
@@ -1,7 +1,7 @@
 
 class ProjectInfo():
-    AZURE_DEVOPS_URI = ""
-    AZURE_DEVOPS_PROJECT_NAME = ""
-    AZURE_DEVOPS_REPOSITORY_NAME = ""
-    AZURE_OPEN_AI_ENDPOINT = ""
-    PULL_REQUEST_ID = 1
+    AZURE_DEVOPS_URI = "https://dev.azure.com/Microsoft"
+    AZURE_DEVOPS_PROJECT_NAME = "WDATP"
+    AZURE_DEVOPS_REPOSITORY_NAME = "Mgmt.Resources"
+    AZURE_OPEN_AI_ENDPOINT = "https://foundational-code-review.openai.azure.com/"
+    PULL_REQUEST_ID = 9329278
